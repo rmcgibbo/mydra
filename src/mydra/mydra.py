ae105a8ec3ac153eaeb7e8a15dc91d92634e62fe
Robert T. McGibbon <rmcgibbo@gmail.com>
2021-01-03 00:24:04 -0500

    One stage

--- src/mydra/mydra.py
+++ src/mydra/mydra.py
@@ -157,7 +157,7 @@ def _build_uncached(
     cmd = ["build", "--no-link", "--keep-going"]
     if not sys.stdout.isatty():
         cmd += ["--print-build-logs"]
-    print(f"Building {drvs})
+    print(f"Building {drvs}")
     build_process = pexpect.spawn(
         "nix",
         cmd + drvs,
